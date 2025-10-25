8e21f24f478be6dcdee4a7f9cb6fcef8eca95329
Leandro Jacome <leandrojacome@wposs.com>
2025-10-25 10:14:01 -0500

    first commit

---
+++ src/main/java/com/bian/mail/BianMailApplication.java
@@ -0,0 +1,13 @@
+package com.bian.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BianMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BianMailApplication.class, args);
+    }
+
+}
