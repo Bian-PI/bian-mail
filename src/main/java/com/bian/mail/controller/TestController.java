28263b50e45210f2356e590419909fddfdbe221e
Leandro Jacome <leandrojacome@wposs.com>
2025-11-24 11:51:37 -0500

    Prueba

---
+++ src/main/java/com/bian/mail/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bian.mail.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test() {
+        return "<h1>test</h1>";
+    }
+}
